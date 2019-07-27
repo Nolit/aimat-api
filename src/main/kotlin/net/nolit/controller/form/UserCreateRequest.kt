b501ff2a7bd7d8d6629fedec83e4636c638f90bd
Nolit <yaboonjapan@gmail.com>
2019-07-27 13:19:50 +0900

    Emailバリデーションの追加

--- src/main/kotlin/net/nolit/controller/form/UserCreateRequest.kt
+++ src/main/kotlin/net/nolit/controller/form/UserCreateRequest.kt
@@ -1,9 +1,11 @@
 package net.nolit.controller.form
 
+import javax.validation.constraints.Email
 import javax.validation.constraints.NotBlank
 
 class UserCreateRequest {
     @NotBlank
+    @Email
     lateinit var email: String
     @NotBlank
     lateinit var password: String
