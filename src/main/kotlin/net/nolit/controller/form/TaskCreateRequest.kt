b873fb68bff14cdfb0abae8ebb38127d5ffe35ab
Nolit <yaboonjapan@gmail.com>
2019-08-02 23:59:59 +0900

    バリデーションメッセージの形式変更

--- src/main/kotlin/net/nolit/controller/form/TaskCreateRequest.kt
+++ src/main/kotlin/net/nolit/controller/form/TaskCreateRequest.kt
@@ -3,14 +3,14 @@ package net.nolit.controller.form
 import javax.validation.constraints.*
 
 class TaskCreateRequest {
-    @NotBlank
+    @NotBlank(message = "タイトルを入力してください")
     lateinit var title: String
 
-    @NotBlank
+    @NotBlank(message = "実施日を選択してください")
     lateinit var dueDate: String
     var amount: Int = 0
 
-    @NotBlank
+    @NotBlank(message = "タスクの種類が正しくありません")
     @Pattern(regexp = "[012]")
     lateinit var type: String
 }
